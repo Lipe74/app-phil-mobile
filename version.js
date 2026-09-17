d7d0161e35aacbc0f8a1c678953c3eb3f1d08db5
Phil-Mobile <formaphil@gmail.com>
2026-09-17 10:59:21 +0200

    Frais manuels, export secours, UI produits
    Retrait de l'ajout automatique des 39€ de frais d'activation (devient une ligne manuelle comme les autres frais uniques). Nouveau bouton d'export (produits + promotions) dans l'admin, qui génère le code de secours à jour et le copie dans le presse-papier. Amélioration de l'affichage des lignes produits (Visible/Frais unique/Supprimer alignés horizontalement).

--- version.js
+++ version.js
@@ -4,5 +4,5 @@
 
 (function () {
   var scope = (typeof window !== 'undefined') ? window : self;
-  scope.APP_VERSION = "14.16 - Frais uniques & Synchro live";
+  scope.APP_VERSION = "14.17 - Frais manuels & Export secours";
 })();
